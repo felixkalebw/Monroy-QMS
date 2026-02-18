77fb0f71ff270da93934af7277054dfe71d9515d
felixkalebw <felixkale@gmail.com>
2026-02-18 16:44:54 +0200

    Create Clients.jsx

---
+++ public/src/app/pages/Clients.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Clients() {
+  return (
+    <div style={{ background: "white", padding: 16, borderRadius: 14 }}>
+      <h2 style={{ marginTop: 0 }}>Clients (CRM)</h2>
+      <p>Next: client registration, sites register, contacts register, contracts + uploads.</p>
+    </div>
+  );
+}
