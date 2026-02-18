e131f8a7419a3ea2952d6ab4ae6065b76f860d6b
felixkalebw <felixkale@gmail.com>
2026-02-18 16:46:38 +0200

    Create Ncr.jsx

---
+++ public/src/app/pages/Ncr.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Ncr() {
+  return (
+    <div style={{ background: "white", padding: 16, borderRadius: 14 }}>
+      <h2 style={{ marginTop: 0 }}>NCR</h2>
+      <p>Next: NCR register, assignment, closure evidence, ageing dashboard.</p>
+    </div>
+  );
+}
