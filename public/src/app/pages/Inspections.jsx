4646990dc77d1bcebebab4855fe65778c34f1f13
felixkalebw <felixkale@gmail.com>
2026-02-18 16:45:55 +0200

    Create Inspections.jsx

---
+++ public/src/app/pages/Inspections.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Inspections() {
+  return (
+    <div style={{ background: "white", padding: 16, borderRadius: 14 }}>
+      <h2 style={{ marginTop: 0 }}>Inspections</h2>
+      <p>Next: create inspection, upload photos, certificate issue, next due date updates.</p>
+    </div>
+  );
+}
