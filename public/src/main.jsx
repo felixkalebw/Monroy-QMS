9813e833ad7dcd520c802b9ba4ac445bca472055
felixkalebw <felixkale@gmail.com>
2026-02-19 16:11:10 +0200

    Update main.jsx

--- public/src/main.jsx
+++ public/src/main.jsx
@@ -1,12 +1,10 @@
+// public/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./app/App.jsx";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
