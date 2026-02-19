50dd8af78e57e66c586f4c28587b41b7a3081b58
felixkalebw <felixkale@gmail.com>
2026-02-19 16:22:49 +0200

    Create api.js

---
+++ public/src/lib/api.js
@@ -0,0 +1,16 @@
+// FORCE DEMO MODE ALWAYS
+export function isDemoMode() {
+  return true;
+}
+
+export async function apiGet() {
+  return {};
+}
+
+export async function apiPost() {
+  return {};
+}
+
+export async function apiPut() {
+  return {};
+}
