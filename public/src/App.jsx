81e54a8535e977e47962176a7d2df02052dee3a1
felixkalebw <felixkale@gmail.com>
2026-02-19 16:21:40 +0200

    Update App.jsx

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,7 +1,5 @@
-// public/src/App.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import NcrStatutoryReport from "./pages/NcrStatutoryReport.jsx";
 
@@ -9,10 +7,9 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/ncr-report" element={<NcrStatutoryReport />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
