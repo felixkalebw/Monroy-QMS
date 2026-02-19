df17c8b0081b94b4b810fef6f1f12561524ee032
felixkalebw <felixkale@gmail.com>
2026-02-19 09:24:03 +0200

    Update prisma.js

--- api/src/prisma.js
+++ api/src/prisma.js
@@ -1,9 +1,2 @@
-import { z } from "zod";
-
-const schema = z.object({
-  DATABASE_URL: z.string().min(10),
-  JWT_ACCESS_SECRET: z.string().min(16),
-  NODE_ENV: z.string().default("production")
-});
-
-export const env = schema.parse(process.env);
+import { PrismaClient } from "@prisma/client";
+export const prisma = new PrismaClient();
